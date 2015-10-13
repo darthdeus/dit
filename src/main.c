0ad76282ecb1a8d9191ed16113fde4d26a3bed71
Jakub Arnold <darthdeus@gmail.com>
2015-10-13 00:22:32 +0000

    Almost a working UDP client

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -12,8 +14,25 @@ void failed(const char* s) {
 }
 
 int main() {
-  int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (s == -1) {
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(3000);
+
+  if (inet_aton("127.0.0.1", &addr.sin_addr) == 0) {
+    failed("inet_aton()");
+  }
+
+  const char* str = "hello";
+
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock == -1) {
     failed("socket()");
   }
+
+  int stat = sendto(sock, str, strlen(str), 0, (struct sockaddr*) &addr, sizeof(addr));
+  if (!stat) {
+    failed("stat()");
+  }
+
 }
